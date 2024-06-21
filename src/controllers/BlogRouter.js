1dd864f8e1690ca91fb23b59cb95e9be525802f0
Willem Gibson <129262358+WillemGibson@users.noreply.github.com>
2024-06-22 09:38:12 +1000

    feat: first commit

---
+++ src/controllers/BlogRouter.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { BlogModel } = require("../models/BlogModel");
+const router = express.Router();
+
+router.get("/", async (request, response, next) => {
+
+	let result = await BlogModel.find({}).exec();
+
+	response.json({
+		message:"Blog router homepage",
+		result: result
+	});
+});
+
+router.get("/:id", (request, response, next) => {
+	response.json({
+		message:"Blog router homepage"
+	});
+});
+
+router.post("/", (request, response, next) => {
+	response.json({
+		message:"Blog router homepage"
+	});
+});
+
+router.put("/", (request, response, next) => {
+	response.json({
+		message:"Blog router homepage"
+	});
+});
+
+router.delete("/", (request, response, next) => {
+	response.json({
+		message:"Blog router homepage"
+	});
+});
+
+module.exports = router;
---
+++ src/models/UserModel.js
@@ -0,0 +1,25 @@
+/**
+- username
+- blog post view history
+**/
+
+const mongoose = require("mongoose");
+
+const userSchema - mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    viewHistory: {
+        type: [{type: mongoose.Schema.Types.ObjectId, ref: "Blog"}],
+        required: false,
+        unique: false
+    }
+})
+
+const userModel =  mongoose.model("User", userSchema);
+
+module.exports = {
+    UserModel
+}

fa24fb484134013e22a3d0ff257f79e59796cfb2
Willem Gibson <129262358+WillemGibson@users.noreply.github.com>
2024-06-22 10:55:55 +1000

    feat: seed file works, blog router retrieve all data and populate

--- src/controllers/BlogRouter.js
+++ src/controllers/BlogRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { BlogModel } = require("../models/BlogModel");
+const { UserModel } = require("../models/UserModel");
 const router = express.Router();
 
 router.get("/", async (request, response, next) => {
 
-	let result = await BlogModel.find({}).exec();
+	let result = await BlogModel.find({}).populate("author").exec();
 
 	response.json({
 		message:"Blog router homepage",
--- src/utils/seed.js
+++ src/utils/seed.js
@@ -1,8 +1,38 @@
+const { BlogModel } = require("../models/BlogModel");
 const { UserModel } = require("../models/UserModel");
 const { databaseConnect, databaseClose } = require("./database"); 
 
 async function seedUsers() {
-    
+    let blogData = [
+        {
+            title: "Super Cool Blog Post",
+            content: "Pretend this is 300 words.",
+            author: usersToUse[0].id,
+            headerImage: "https://placehold.com/600x400/EEE/31343C",
+            tags: ["seeded", "blog", "pokemon", "cool beans"],
+            categories: ["coding", "travel"]
+        },
+        {
+            title: "Another Cool Blog Post",
+            content: "Pretend this is 300 words.",
+            author: usersToUse[1].id,
+            headerImage: "https://placehold.com/600x400/EEE/31343C",
+            tags: ["seeded", "tada!", "pokemon", "food"],
+            categories: ["photography", "life"]
+        },
+        {
+            title: "The Third Cool Blog Post",
+            content: "Pretend this is 300 words.",
+            author: usersToUse[1].id,
+            headerImage: "https://placehold.com/600x400/EEE/31343C",
+            tags: ["seeded", "tada!", "pokemon", "food"],
+            categories: ["photography", "life"]
+        }
+    ]
+
+    let result = await BlogModel.insertMany(blogData);
+    console.log(result);
+    return result;
 }
 
 async function seedUsers() {
